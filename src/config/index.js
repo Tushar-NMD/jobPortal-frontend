acb6a3bda9d8fcbb079ff87c99d8b26c62276c1a
Delta student <149225920+Tushar123097@users.noreply.github.com>
2025-11-16 22:56:40 +0530

    Fix Vercel deployment routing

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const conf = {
-    apiBaseUrl: import.meta.env.VITE_API_BASE_URL
+    apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'https://jobportal-backend-1-30wj.onrender.com'
 };
 
 export default conf;
